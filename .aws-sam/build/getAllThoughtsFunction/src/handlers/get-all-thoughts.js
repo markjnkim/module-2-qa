d978a198f87380e08b118d3c20954ba1678a7c66
markjnkim <markjnkim@gmail.com>
2021-04-30 15:42:51 -0700

    lesson three complete

--- .aws-sam/build/getAllThoughtsFunction/src/handlers/get-all-thoughts.js
+++ .aws-sam/build/getAllThoughtsFunction/src/handlers/get-all-thoughts.js
@@ -1,18 +1,33 @@
- exports.getAllThoughtsHandler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-      throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+
+exports.getAllThoughtsHandler = async (event) => {
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+    );
   }
   // All log statements are written to CloudWatch
-  console.info('RECEIVED:', event);
+  console.info("received:", event);
+
+  const params = {
+    TableName: tableName,
+    ScanIndexForward: true,
+  };
+  const data = await docClient.scan(params).promise();
+  const items = data.Items;
 
   const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-      message: 'get all thoughts'
-      })
+    statusCode: 200,
+    body: JSON.stringify(items),
   };
 
   // All log statements are written to CloudWatch
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+
   return response;
-}
+};
--- .aws-sam/build/getByUserFunction/src/handlers/put-thought.js
+++ .aws-sam/build/getByUserFunction/src/handlers/put-thought.js
@@ -1,18 +1,39 @@
- exports.putThoughtHandler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-      throw new Error(`putThought only accept POST method, you tried: ${event.httpMethod}`);
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+exports.putThoughtHandler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    throw new Error(
+      `putThought only accept POST method, you tried: ${event.httpMethod}`
+    );
   }
   // All log statements are written to CloudWatch
-  console.info('RECEIVED:', event);
+  console.info("received:", event);
+
+  // Get username and thought from the body of the request
+  const body = JSON.parse(event.body);
+  const username = body.username;
+  const thought = body.thought;
+  const rightNow = Date.now().toString();
+
+  const params = {
+    TableName: tableName,
+    Item: { username: username, thought: thought, createdAt: rightNow },
+  };
+
+  docClient.put(params).promise();
 
   const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-      message: 'put thought'
-      })
+    statusCode: 200,
+    body: JSON.stringify(body)
   };
 
   // All log statements are written to CloudWatch
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+
   return response;
-}
+};
