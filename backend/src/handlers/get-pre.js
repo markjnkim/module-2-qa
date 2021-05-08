f3a9aa490c945b6c7fddb66226c1c48353208e2a
markjnkim <markjnkim@gmail.com>
2021-05-08 12:49:43 -0700

    retrieve presigned URL

--- backend/src/handlers/get-pre.js
+++ backend/src/handlers/get-pre.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.AWS_REGION });
 const s3 = new AWS.S3();
 
@@ -14,35 +14,44 @@ exports.getPreUrlHandler = async (event, context) => {
   // };
 
   // return response;
-  return await getUploadURL(event)
+  return await getUploadURL(event);
 };
 
-const getUploadURL = async function(event) {
+const getUploadURL = async function (event) {
   const randomID = parseInt(Math.random() * 10000000);
   const Key = `${randomID}.jpg`;
-  console.log(randomID, Key)
 
   // Get signed URL from S3
   const s3Params = {
     Bucket: process.env.UPLOAD_BUCKET,
     Key,
     Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
-    // This ACL makes the uploaded object publicly readable. 
-    ACL: 'public-read'
+    ContentType: "image/jpeg",
+    // This ACL makes the uploaded object publicly readable.
+    ACL: "public-read",
   };
 
-  console.log('Params: ', s3Params);
-  try {
-    const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params);
-    console.log('uploadURL: ', uploadURL);
-  } catch(err) {
-    console.error(err);
-  }
+  const uploadURL = await s3.getSignedUrlPromise("putObject", s3Params);
+  const response = {
+    body: JSON.stringify({
+      Key: Key,
+      uploadURL: uploadURL,
+    })
+  };
 
-  return JSON.stringify({
-    uploadURL: uploadURL,
-    Key,
-  });
+  console.info("Params: ", s3Params);
+  console.info("response: ", response);
+
+  return response;
 };
 
+
+// const response = {
+//   statusCode: 200,
+//   headers: {
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+//   },
+//   body: JSON.stringify(Items),
+// };
